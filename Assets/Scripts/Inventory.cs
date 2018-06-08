b85a1a8edc4a0806d71ed20ebb67b57cd18619e1
rupertgaray <rupert.garay@gmail.com>
2018-06-08 14:05:33 -0300

    novo update fix

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour, IHasChanged
+{
+
+    [SerializeField] public Transform slots;
+    private string texto;
+    
+
+    void Start()
+    {
+        HasChanged();
+    }
+
+    #region IHasChanged implementation
+    public void HasChanged()
+    {
+        System.Text.StringBuilder builder = new System.Text.StringBuilder();
+        //builder.Append(" - ");
+        foreach (Transform slotTransform in slots)
+        {
+            GameObject item = slotTransform.GetComponent<Slot>().item;
+            if (item)
+            {
+                builder.Append(item.name);
+                builder.Append(" - ");
+            }
+        }
+        texto = builder.ToString();
+        //Debug.Log(texto);
+        ListarMovimentos();
+    }
+    #endregion
+
+    public string[] ListarMovimentos()
+    {
+        string[] mov = texto.Split('-');
+        /*foreach(string word in mov)
+        {
+            Debug.Log(word);
+        }*/
+        
+        return mov;
+    }
+}
+
+
+namespace UnityEngine.EventSystems
+{
+    public interface IHasChanged : IEventSystemHandler
+    {
+        void HasChanged();
+    }
+}
