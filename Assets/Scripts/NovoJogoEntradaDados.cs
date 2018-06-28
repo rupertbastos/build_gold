119b130bcfa6b660ad098844ba8f0babec65cbe9
Guilherme Galarça Dutra <udutra@gmail.com>
2018-06-28 20:34:35 -0300

    Entradas de dados 50%

--- Assets/Scripts/NovoJogoEntradaDados.cs
+++ Assets/Scripts/NovoJogoEntradaDados.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NovoJogoEntradaDados : MonoBehaviour {
 
-    public string nome, sobreNome;
+    public Text nome;
+    public GameObject canvasNome, canvasAvatar;
 
 
     public void AcaoBotaoNovoJogoEntrada_Dados(string opcao)
@@ -17,8 +19,17 @@ public class NovoJogoEntradaDados : MonoBehaviour {
                     //SceneManager.LoadScene("03_01_SelectTowers");
 
 
-
-                    Debug.LogWarning("Nome: " + nome + " " + sobreNome);
+                    if(nome.text.ToString().Length > 0)
+                    {
+                        Debug.LogWarning("Nome: " + nome.text.ToString().ToUpper());
+                        canvasNome.SetActive(false);
+                        canvasAvatar.SetActive(true);
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Nome não pode ficar em branco!");
+                    }
+                    
                     break;
                 }
             case "Voltar":
