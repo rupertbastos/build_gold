a2c697a0a3e12a0fff7177356db1212125c2ec02
RUPERT-NOTEDELL\ruper <rupert.garay@gmail.com>
2018-07-07 21:42:42 -0300

    update: sound and animations

---
+++ Assets/Scripts/sounds.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sounds : MonoBehaviour {
+
+    // Source Audio Player
+
+    public AudioClip mover;
+    public AudioClip pular;
+    public AudioClip laserSaindo;
+    public AudioClip ataqueDeContato;
+    public AudioClip subirDescerEscada;
+    public AudioClip caindo;
+    public AudioClip esmagado;
+    public AudioClip arremessado;
+    public AudioClip ________________________________NadaApenasUmDivisor;
+
+
+    // Source Audio Armadilhas
+
+    public AudioClip madeiraDesmanchando;
+    public AudioClip concretoCaindo;
+    public AudioClip geloCongelando;
+    public AudioClip cristalQuebrando;
+    public AudioClip _______________________________nadaAquiApenasDivisor;
+
+
+    // Source Audio HUD Gameplay
+
+    public AudioClip uparNivel;
+    public AudioClip perdeuVidaNaTentativa;
+    public AudioClip botaoListaMetodos;
+    public AudioClip botaoPlay;
+    public AudioClip slideTutotial;
+    public AudioClip notificacaoDeTrofeu;
+    public AudioClip _______________________________nadaApenasDivisor;
+
+
+    // Source Audio Navegação
+
+    public AudioClip botaoIr;
+    public AudioClip botaoVoltar;
+    public AudioClip botaoSetas;
+    public AudioClip botaoHighlited;
+    public AudioClip botaoPause;
+    public AudioClip botaoTorres;
+    public AudioClip botaoFases;
+    public AudioClip botaoEscolherAvatar;
+    public AudioClip botaoEscolherCor;
+    public AudioClip botaoSalvarPerfil;
+
+    public AudioClip ____________________nadaAquiApenasUmDivisor;
+
+
+    // Source Audio Ambiente
+
+    public AudioClip mainMenu;
+    public AudioClip fasesTorreMadeira;
+    public AudioClip fasesTorreConcreto;
+    public AudioClip fasesTorreJade;
+    public AudioClip fasesTorreGelo;
+    public AudioClip telaSucessos;
+    public AudioClip telaGameOver;
+
+}
